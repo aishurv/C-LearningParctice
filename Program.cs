d515d5a19f53297d19faae9fb70136e5198c71ae
Laxmi Wandare <Laxmi.wandare@klingelenberg.com>
2024-12-24 12:32:24 +0530

    Static Constructor Demonstation Added.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.ComponentModel.DataAnnotations;
-using PracticeProject.EmpMgmt;
-using OnlineAuctionSys;
+//using PracticeProject.EmpMgmt;
+//using OnlineAuctionSys;
+using oop;
 
-//dataTypes dt = new dataTypes(); // To check default values use new dataTypes(1);
-//dt.checkdatatypes();
+/*
+dataTypes dt = new dataTypes(); // To check default values use new dataTypes(1);
+dt.checkdatatypes();
 
-//dataTypes.parseVsTryParse();
-//dataTypes.strBehaviour();
+dataTypes.parseVsTryParse();
+dataTypes.strBehaviour();
+*/
 
 //arrayAndCollections.array(); 
 
 //EmpMgmtProject empMgmtProject = new EmpMgmtProject();
 
-OnlineAuctionSystem sample = new OnlineAuctionSystem();
+//OnlineAuctionSystem sample = new OnlineAuctionSystem();
+
+
+/* // Static Construtor 
+staticConstructor.sample();
+Console.WriteLine("obj 1 initilization start !");
+var obj1 = new staticConstructor();
+Console.WriteLine("obj 1 initilization end !");
+
+Console.WriteLine("obj 2 initilization start !");
+var obj2 = new staticConstructor();
+Console.WriteLine("obj 2 initilization end !");
+
+Console.WriteLine("obj 3 initilization start !");
+var obj3 = new staticConstructor();
+Console.WriteLine("obj 3 initilization end !");
+*/
+
+var s1 = Singleton.Instance;
+var s =  Singleton1.obj;
---
+++ oop/StaticConstructor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace oop
+{
+    public class staticConstructor
+    {
+        static staticConstructor()
+        {
+            Console.WriteLine("static Constructor called !");
+        }
+        public staticConstructor()
+        {
+            Console.WriteLine("public Constructor Invoked !");
+        }
+        public static void sample()
+        {
+            Console.WriteLine("Sample Static Method called !");
+
+        }
+    }
+    public class Singleton
+    {
+        // Static field initializer calls instance constructor.
+        private static Singleton instance = new Singleton();
+
+        private Singleton()
+        {
+            Console.WriteLine("Executes before static constructor. private constructor");
+        }
+
+        static Singleton()
+        {
+            Console.WriteLine("Executes after instance constructor. static constructor");
+        }
+        public static Singleton Instance => instance;
+    }
+
+    public class Singleton1
+    {
+        public static Singleton1 obj1 = new Singleton1();
+        public Singleton1()
+        {
+            Console.WriteLine("Executes before static constructor. Public constructor");
+        }
+
+        static Singleton1()
+        {
+            Console.WriteLine("Executes after instance constructor. static constructor");
+        }
+        public static Singleton1 obj => obj1;
+    }
+}
